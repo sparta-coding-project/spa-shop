2cb8fb27ff64c8e585c5487f46e5358ff6d30b96
lyukidon <qgx99098@gmail.com>
2024-01-23 15:35:38 +0900

    [feat] mongoose schema - add enumeration

--- schemas/products.schema.js
+++ schemas/products.schema.js
@@ -4,30 +4,31 @@ const { Schema } = mongoose;
 
 // defaultSchema를 정의합니다.
 const productsSchema = new Schema({
-    title: {
-      type: String,
-      required: true,
-    },
-    content: {
-      type: String,
-      required: true,
-    },
-    author: {
-      type: String,
-      required: true,
-    },
-    isSales: {
-      type: String,
-      required: true,
-      default: "FOR_SALE"
-    },
-    password: {
-      type: String,
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-    },
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  isSales: {
+    type: String,
+    required: true,
+    enum: ["FOR_SALE", "SOLD_OUT"],
+    default: "FOR_SALE",
+  },
+  password: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 // defaultSchema를 사용하여 'Defaults'라는 이름의 mongoose 모델을 생성합니다.
